e0bb7a67930d0e2635818653d934701da9e6e6f1
Andy(앤디/이진원) <77874587+wlsdnjs829@users.noreply.github.com>
2023-01-01 13:56:08 +0900

    Time: 318 ms (95.72%), Space: 40.5 MB (57.91%) - LeetHub

--- 0055-jump-game/0055-jump-game.kt
+++ 0055-jump-game/0055-jump-game.kt
@@ -1,24 +1,18 @@
 class Solution {
     fun canJump(nums: IntArray): Boolean {
-        var current = 0
-        var index = 1
-
-        val size = nums.size - 1
-
-        repeat(size + 1) {
-            when {
-                it == 0 && nums[it] > 0 -> current = nums[it]
-                current >= size -> return@canJump true
-                index > current -> return@canJump false
-                nums[index] == 0 -> index++
-                index + nums[index] >= current -> {
-                    current = nums[index] + index
-                    index += 1
-                }
-                else -> index++
+        var maxIndex = 0
+        var i = 0
+    
+        while (i <= maxIndex) {
+            maxIndex = maxOf(maxIndex, i + nums[i])
+            
+            if (maxIndex >= nums.size - 1) {
+                return true
             }
+            
+            ++i
         }
-
-        return size == 0
+        
+        return false
     }
 }
