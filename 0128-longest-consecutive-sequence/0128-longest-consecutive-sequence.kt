ae0999e2e5e7af2ef56d73aad891cd54baa83cad
Andy(앤디/이진원) <77874587+wlsdnjs829@users.noreply.github.com>
2023-03-25 13:00:21 +0900

    Time: 414 ms (96.05%), Space: 48.8 MB (97.37%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.kt
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.kt
@@ -1,21 +1,21 @@
 class Solution {
     fun longestConsecutive(nums: IntArray): Int {
-        val numberSet: Set<Int> = nums.toSet()
-
-        var maxConsequtive = 0
-        numberSet.forEach { number ->
-            if (numberSet.contains(number - 1).not()) {
-                var currentNumber = number
-                var currentConsequtive = 1
-                while (numberSet.contains(currentNumber + 1)) {
-                    currentNumber++
-                    currentConsequtive++
-                }
-
-                maxConsequtive = Math.max(maxConsequtive, currentConsequtive)
+        if (nums.size == 0) {
+            return 0
+        }
+        nums.sort()
+        var max = 1
+        var longestFound = 1
+        for (i in 1 until nums.size) {
+            if (nums[i - 1] == nums[i] - 1) {
+                longestFound++
+            } else if (nums[i - 1] == nums[i]) {
+                continue
+            } else { 
+                max = Math.max(max, longestFound)
+                longestFound = 1
             }
         }
-
-        return maxConsequtive
+        return Math.max(max, longestFound)
     }
 }
