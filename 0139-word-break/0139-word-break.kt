01f2cff6ce410d00debd80e1bcbdb0efd5ea8dba
Andy(앤디/이진원) <77874587+wlsdnjs829@users.noreply.github.com>
2023-04-08 12:43:37 +0900

    Time: 191 ms (16.28%), Space: 37.8 MB (5.43%) - LeetHub

--- 0139-word-break/0139-word-break.kt
+++ 0139-word-break/0139-word-break.kt
@@ -1,12 +1,12 @@
 class Solution {
     fun wordBreak(s: String, wordDict: List<String>): Boolean {
         val dp = BooleanArray(s.length + 1) { it == 0 }
-        val map = wordDict.associateWith { true }
 
         for (index in dp.indices) {
             for (next in 0..index) {
-                if (dp[next] && map[s.substring(next until index)] == true) {
+                if (dp[next] && wordDict.contains(s.substring(next until index))) {
                     dp[index] = true
+                    break
                 }
             }
         }
