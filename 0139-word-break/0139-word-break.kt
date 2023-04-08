4c5878a643b5c03df29950ed83b9de7680c8e5d5
Andy(앤디/이진원) <77874587+wlsdnjs829@users.noreply.github.com>
2023-04-08 12:37:44 +0900

    Time: 203 ms (9.30%), Space: 37.6 MB (5.43%) - LeetHub

---
+++ 0139-word-break/0139-word-break.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun wordBreak(s: String, wordDict: List<String>): Boolean {
+        val dp = BooleanArray(s.length + 1) { it == 0 }
+
+        for (index in dp.indices) {
+            for (next in 0..index) {
+                if (dp[next] && wordDict.contains(s.substring(next until index))) {
+                    dp[index] = true
+                }
+            }
+        }
+
+        return dp[dp.lastIndex]
+    }
+}
