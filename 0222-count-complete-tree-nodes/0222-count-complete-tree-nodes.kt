bbd02404e53e5f01f2a9b63304840df54c68adb5
Andy(앤디/이진원) <77874587+wlsdnjs829@users.noreply.github.com>
2023-06-04 20:46:46 +0900

    Time: 219 ms (62.16%), Space: 39 MB (91.89%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.kt
@@ -0,0 +1,22 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun countNodes(root: TreeNode?): Int {
+        if (root == null) {
+            return 0
+        }
+
+        val left = countNodes(root = root.left)
+        val right = countNodes(root = root.right)
+
+        return left + right + 1
+    }
+}
