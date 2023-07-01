110375631e0314860a683890a5454c897038715e
Andy(앤디/이진원) <77874587+wlsdnjs829@users.noreply.github.com>
2023-07-01 13:59:43 +0900

    Time: 165 ms (90.72%), Space: 34.6 MB (94.85%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.kt
+++ 0010-regular-expression-matching/0010-regular-expression-matching.kt
@@ -1,16 +1,30 @@
 class Solution {
     fun isMatch(s: String, p: String): Boolean {
-        if (p.isEmpty()) {
-            return s.isEmpty()
+        if (s == null || p == null) return false
+        val lenS = s.length
+        val lenP = p.length
+
+        var dp = Array(lenS+1) {BooleanArray(lenP+1) {false}}
+        dp[0][0] = true
+        for (i in 0 until lenP) {
+            if(p[i] == '*' && dp[0][i - 1]) {
+                dp[0][i+1] = true
+            }
         }
 
-        val isFirstMatch = s.isNotEmpty() && listOf(s.first(), '.').contains(p.first())
-
-        return if (p.length >= 2 && p[1] == '*') {
-            isMatch(s, p.substring(2, p.length)) ||
-                    isFirstMatch && isMatch(s.substring(1, s.length), p)
-        } else {
-            isFirstMatch && isMatch(s.substring(1, s.length), p.substring(1, p.length))
+        for(i in 0 until lenS) {
+            for (j in 0 until lenP) {
+                if (p[j] == '.' || p[j] == s[i]) {
+                    dp[i+1][j+1] = dp[i][j]
+                } else if(p[j] == '*') {
+                    if (p[j - 1] != s[i] && p[j-1] != '.') {
+                        dp[i+1][j+1] = dp[i+1][j-1]
+                    } else {
+                        dp[i+1][j+1] = dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1]
+                    }
+                }                
+            }
         }
+        return dp[lenS][lenP]
     }
 }
